7e4eba34ace6fa5cc6400e75d2d235d91089932f
Jules59 <julemaire01@gmail.com>
2018-06-06 12:35:42 +0200

    replace deconnection by déconnection

---
+++ src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 06/06/18
+ * Time: 10:10
+ */
+
+namespace AppBundle\EventListener;
+
+
+class RedirectAfterRegistrationSubscriber
+{
+
+}
