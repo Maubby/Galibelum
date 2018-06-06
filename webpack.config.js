5fd2924d6a2f0a8f1e82a0b3a3ffd9d3e533c2e8
Sebastien Petit <sebastien.petit59@laposte.net>
2018-06-06 10:23:18 +0200

    font awesome modification

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,12 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     .setOutputPath('web/build/')
-    .setPublicPath('/web')
-    .addEntry('style', './assets/scss/main.scss')
+    .setPublicPath('/build')
+    .addEntry('style', './assets/scss/style.scss')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableBuildNotifications()
     .enableSassLoader()
-    .enableVersioning()
-
+    .enableVersioning();
 
 module.exports = Encore.getWebpackConfig();
