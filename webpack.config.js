736f484f1108dd12ac999864dab51ec0c74219de
chasticot <cannelle.legrand14@gmail.com>
2018-06-06 09:48:49 +0200

    Fix bug on the webpack config, update the scss files

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,11 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('web/build/')
     .setPublicPath('/web')
+    .addEntry('bootstrap', './assets/scss/bootstrap.scss')
     .addEntry('style', './assets/scss/main.scss')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     .enableBuildNotifications()
-    .enableSassLoader()
-    .enableVersioning()
-
+    .enableSassLoader();
 
 module.exports = Encore.getWebpackConfig();
