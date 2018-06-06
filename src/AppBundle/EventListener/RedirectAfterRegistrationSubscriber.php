5d0f1ed3757be93b0225528f7140c35a2f08232e
Jules59 <julemaire01@gmail.com>
2018-06-06 12:37:31 +0200

    creation EventListener to do redirection to the homepage after regestration subscriber

--- src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
+++ src/AppBundle/EventListener/RedirectAfterRegistrationSubscriber.php
@@ -1,15 +1,66 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: julien
- * Date: 06/06/18
- * Time: 10:10
+ * RedirectAfterRegistrationSubscriber File Doc Comment.
+ *
+ * PHP version 7.1
+ *
+ * @category RedirectAfterRegistrationSubscriber
+ * @package  EventListener
+ * @author   WildCodeSchool <contact@wildcodeschool.fr>
  */
 
 namespace AppBundle\EventListener;
 
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\FOSUserEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
 
-class RedirectAfterRegistrationSubscriber
+/**
+ * RedirectAfterRegistrationSubscriber.
+ *
+ * @category RedirectAfterRegistrationSubscriber
+ * @package  EventListener
+ * @author   WildCodeSchool <contact@wildcodeschool.fr>
+ */
+class RedirectAfterRegistrationSubscriber implements EventSubscriberInterface
 {
+    private $_router;
 
+    /**
+     * Construct.
+     *
+     * @param RouterInterface $_router Creation router for a redirection
+     */
+    public function __construct(RouterInterface $_router)
+    {
+        $this->_router = $_router;
+    }
+
+    /**
+     * Redirection to the homepage.
+     *
+     * @param FormEvent $event Event calling on registration
+     *
+     * @return void
+     */
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        $url = $this->_router->generate('homepage');
+        $reponse = new RedirectResponse($url);
+        $event->setResponse($reponse);
+    }
+
+    /**
+     * GetSubscribeEvents.
+     *
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            FOSUserEvents::REGISTRATION_SUCCESS => 'onRegistrationSuccess'
+        ];
+    }
 }
