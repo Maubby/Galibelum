e372ff258e61dced8f7b5b7849f31ab36defb960
serre.thibault@gmail.com <serre.thibault@gmail.com>
2018-06-11 16:38:54 +0200

    add some modification on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,15 @@ let Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('web/build/')
     .setPublicPath('/build')
-    .addEntry('bootstrap', './assets/scss/bootstrap.scss')
-    .addEntry('font-awesome', './assets/scss/font-awesome.scss')
+    .createSharedEntry('vendor', ['jquery','bootstrap-sass', 'bootstrap-sass/assets/stylesheets/_bootstrap.scss',])
     .addStyleEntry('style', './assets/scss/style.scss')
+    .autoProvidejQuery()
     .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
     .enableBuildNotifications()
-    .enableSassLoader()
+    .enableSassLoader(function(sassOptions) {}, {
+        resolveUrlLoader: false
+    })
+    .enableSourceMaps(!Encore.isProduction())
     .enableVersioning();
 
 module.exports = Encore.getWebpackConfig();
