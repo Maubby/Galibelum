df891b0e8dd3a06bad18d4abca776e126f84e9a9
Dine16 <36644777+Dine16@users.noreply.github.com>
2018-06-12 12:01:45 +0200

    Update ProfileType.php

--- src/AppBundle/Form/ProfileType.php
+++ src/AppBundle/Form/ProfileType.php
@@ -16,7 +16,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 /**
- * ProfileController type.
+ * Profile type.
  *
  * @category ProfileType
  * @package  Type
