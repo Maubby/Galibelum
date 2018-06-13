2449f0039ade5df6d11d3163ee96949f4c417a7a
DawnRmax <cannelle.legrand14@gmail.com>
2018-06-13 15:54:21 +0200

    Updated the webpack config to add an entry for the check and confirmed  email scss

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ Encore
     .enableVersioning()
     .createSharedEntry('vendor', ['jquery', 'bootstrap-sass', 'bootstrap-sass/assets/stylesheets/_bootstrap.scss'])
     .addStyleEntry('style', './assets/scss/style.scss')
+    .addStyleEntry('check_confirmed_email', './assets/scss/check_confirmed_email.scss')
     .addEntry('main', './assets/js/main.js')
     // Targeting images repository
     .addPlugin(new CopyWebpackPlugin([
