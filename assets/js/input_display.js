007c74b2a1f06dd1feca2d9f5bf0a1d212c42c33
Dawn Rmax <cannelle.legrand14@gmail.com>
2018-06-22 13:13:37 +0200

    Added a js script for hide and show the address and date inputs

--- assets/js/input_display.js
+++ assets/js/input_display.js
@@ -3,12 +3,32 @@ $(document).ready(function(){
     $(".activity").change(function(){
         // Check if current value is "Évènement eSport"
         if($(this).val() === "Évènement eSport"){
+            // Security client view, input is required
+            $(".activityInfo").prop('required',true);
+            // Remove n/a value when you want to create an event
+            $(".activityInfo").val("");
             // Show input field
-            $(".activityInfo").show();   //This changes display to block
+            $(".activityInfo").show();
         }if($(this).val() === "Equipe eSport"){
+            console.log('trouduc');
+            // Security client view, input is required
+            $(".activityInfo").prop('required',true);
+            // Set n/a value when you want to create a team
+            $(".activityInfo").val("n/a");
             // Hide input field
             $(".activityInfo").hide();
         }if($(this).val() === "Activité de streaming"){
+            // Security client view, input is required
+            $(".activityInfo").prop('required',true);
+            // Set n/a value when you want to create an activity
+            $(".activityInfo").val("n/a");
+            // Hide input field
+            $(".activityInfo").hide();
+        }if($(this).val() === ""){
+            // Security client view, input is required
+            $(".activityInfos").prop('required',true);
+            // Set n/a value
+            $(".activityInfo").val("n/a");
             // Hide input field
             $(".activityInfo").hide();
         }
