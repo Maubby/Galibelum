fc36da221ad25a0e2aab856ca83a96fdc37358f6
Dawn Rmax <cannelle.legrand14@gmail.com>
2018-06-25 13:22:15 +0200

    Updated the security view on the script js

--- assets/js/input_display.js
+++ assets/js/input_display.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
         if($(this).val() === "Évènement eSport") {
             // Security client view, input is required
             $(".activityInfo").prop('required',true);
+            // Security client view, input is required
+            $(".activityAchievement").prop('required',true);
             // Remove n/a value when you want to create an event
             $(".activityInfo").val("");
             // Set n/a value when you want to create an event
@@ -14,11 +16,13 @@ $(document).ready(function() {
             // Hide input field
             $(".activityAchievement").hide();
         }
-        
+
         if($(this).val() === "Equipe eSport") {
             console.log('trouduc');
             // Security client view, input is required
             $(".activityInfo").prop('required',true);
+            // Security client view, input is required
+            $(".activityAchievement").prop('required',true);
             // Set n/a value when you want to create a team
             $(".activityInfo").val("n/a");
             // Remove n/a value when you want to create a team
@@ -28,10 +32,12 @@ $(document).ready(function() {
             // Show input field
             $(".activityAchievement").show();
         }
-        
+
         if($(this).val() === "Activité de streaming") {
             // Security client view, input is required
             $(".activityInfo").prop('required',true);
+            // Security client view, input is required
+            $(".activityAchievement").prop('required',true);
             // Set n/a value when you want to create an activity
             $(".activityInfo").val("n/a");
             $(".activityAchievement").val("n/a");
@@ -39,10 +45,12 @@ $(document).ready(function() {
             $(".activityInfo").hide();
             $(".activityAchievement").hide();
         }
-        
+
         if($(this).val() === "") {
             // Security client view, input is required
             $(".activityInfos").prop('required',true);
+            // Security client view, input is required
+            $(".activityAchievement").prop('required',true);
             // Set n/a value
             $(".activityInfo").val("n/a");
             $(".activityAchievement").val("n/a");
