4a0585fda1543cd076f284b26919c760aa5b2166
Dawn Rmax <cannelle.legrand14@gmail.com>
2018-06-29 14:20:25 +0200

    Added the Eventlistener for file upload

--- src/AppBundle/EventListener/FileUploadListener.php
+++ src/AppBundle/EventListener/FileUploadListener.php
@@ -8,8 +8,48 @@
 
 namespace AppBundle\EventListener;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use AppBundle\Entity\Activity;
+use AppBundle\Service\FileUploaderService;
 
 class FileUploadListener
 {
+    private $uploader;
 
+    public function __construct(FileUploaderService $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        $this->uploadFile($entity);
+    }
+
+    private function uploadFile($entity)
+    {
+        // upload only works for Activity entities
+        if (!$entity instanceof Activity) {
+            return;
+        }
+
+        $file = $entity->getUploadPdf();
+
+        // only upload new files
+        if ($file instanceof UploadedFile) {
+            $fileName = $this->uploader->upload($file);
+            $entity->setUploadPdf($fileName);
+        }
+    }
 }
