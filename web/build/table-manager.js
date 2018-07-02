f7d4fe0f02c740a3fe00d95ca914a9b9f1024f63
Julien Canis <jcanis18@gmail.com>
2018-07-02 16:42:11 +0200

    run npm

--- web/build/table-manager.js
+++ web/build/table-manager.js
@@ -1,1 +1,19 @@
-webpackJsonp([17],{"7x0W":function(a,n,t){(function(a){a(document).ready(function(){a("#table-manager").DataTable()})}).call(n,t("7t+N"))}},["7x0W"]);
+webpackJsonp([17],{
+
+/***/ "./assets/js/table-manager.js":
+/*!************************************!*\
+  !*** ./assets/js/table-manager.js ***!
+  \************************************/
+/*! dynamic exports provided */
+/*! all exports used */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function($) {$(document).ready(function () {
+    $("#table-manager").DataTable();
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js")))
+
+/***/ })
+
+},["./assets/js/table-manager.js"]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvanMvdGFibGUtbWFuYWdlci5qcyJdLCJuYW1lcyI6WyIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsIkRhdGFUYWJsZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7OztBQUFBLHlDQUFBQSxFQUFFQyxRQUFGLEVBQVlDLEtBQVosQ0FBa0IsWUFBVztBQUN6QkYsTUFBRSxnQkFBRixFQUFvQkcsU0FBcEI7QUFDSCxDQUZELEUiLCJmaWxlIjoidGFibGUtbWFuYWdlci5qcyIsInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCkge1xuICAgICQoXCIjdGFibGUtbWFuYWdlclwiKS5EYXRhVGFibGUoKTtcbn0pO1xuXG5cbi8vIFdFQlBBQ0sgRk9PVEVSIC8vXG4vLyAuL2Fzc2V0cy9qcy90YWJsZS1tYW5hZ2VyLmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
