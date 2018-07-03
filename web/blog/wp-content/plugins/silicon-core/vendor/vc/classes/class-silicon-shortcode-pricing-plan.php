ad8f4a450ec3c68cb5bf546353f5eca57c585910
Julien Canis <jcanis18@gmail.com>
2018-07-03 17:26:08 +0200

    delete dir blog

--- web/blog/wp-content/plugins/silicon-core/vendor/vc/classes/class-silicon-shortcode-pricing-plan.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Pricing Plan | silicon_pricing_plan
- *
- * @author 8guild
- */
-class Silicon_Shortcode_Pricing_Plan extends WPBakeryShortCode {}
--- web/blog/wp-content/plugins/silicon-core/vendor/vc/classes/class-silicon-shortcode-testimonials-carousel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Testimonials Carousel | silicon_testimonials_carousel
- *
- * @author 8guild
- */
-class Silicon_Shortcode_Testimonials_Carousel extends Silicon_Shortcode_Container {}
