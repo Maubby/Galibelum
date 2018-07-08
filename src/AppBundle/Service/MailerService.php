942fe11d35c76cc0d7affbd45b77738a4059ad35
Jules59 <julemaire01@gmail.com>
2018-07-08 20:35:51 +0200

    Creation of one service mailer

--- src/AppBundle/Service/MailerService.php
+++ src/AppBundle/Service/MailerService.php
@@ -17,17 +17,20 @@ namespace AppBundle\Service;
  * @package  Service
  * @author   WildCodeSchool <contact@wildcodeschool.fr>
  */
-class Mailer
+class MailerService
 {
-    protected $mailer;
+    protected $mailerUser;
     protected $templating;
     /**
-     * @param \Swift_Mailer $mailer
-     * @param \Twig_Environment $templating
+     * Send an email
+     *
+     * @param \Swift_Mailer     $mailerUser SwiftMailer
+     * @param \Twig_Environment $templating Use template twig
      */
-    public function __construct(\Swift_Mailer $mailer, \Twig_Environment $templating)
-    {
-        $this->mailer     = $mailer;
+    public function __construct(
+        \Swift_Mailer $mailerUser, \Twig_Environment $templating
+    ) {
+        $this->mailerUser     = $mailerUser;
         $this->templating = $templating;
     }
 
@@ -35,17 +38,18 @@ class Mailer
      * Send an email with recipients
      *
      * @param string $fromEmail Sender's email
-     * @param string $toEmail Receiver's email
-     * @param string $recipientName Receiver's name
-     * @param string $subject Email's subject
-     * @param string $message Email's body
+     * @param string $toEmail   Receiver's email
+     * @param string $subject   Email's subject
+     * @param string $message   Email's body
      *
-     * @return int The number of successful recipients. Can be 0 which indicates failure
      * @throws \Twig_Error_Loader
      * @throws \Twig_Error_Runtime
      * @throws \Twig_Error_Syntax
+     *
+     * @return int The number of successful recipients.
+     * Can be 0 which indicates failure
      */
-    public function sendEmail($fromEmail, $toEmail, $recipientName, $subject, $message)
+    public function sendEmail($fromEmail, $toEmail, $subject, $message)
     {
          //If your service is another, then read the following article
          //to know which smpt code to use and which port
@@ -57,14 +61,14 @@ class Mailer
             ->setTo($toEmail)
             ->setBody(
                 $this->templating->render(
-                    'email/contact.html.twig',
-                    array('name' => $recipientName,
-                        'subject' => $subject,
-                        'message' => $message
-                    )
-                ),
-                'text/html'
+                    'email/notification.html.twig',
+                    array(
+                        'message' => $message,
+                        'subject' => $subject
+                        )
+                )
             );
-        return $this->mailer->send($message);
+
+        return $this->mailerUser->send($message);
     }
 }
