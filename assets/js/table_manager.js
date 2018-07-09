bb1fe9b6e71f36cb9b57da5344f000980d8347e3
Dine16 <amandine.claude6@gmail.com>
2018-07-09 10:56:51 +0200

    Deleting double code for table_manager.js

--- assets/js/table_manager.js
+++ assets/js/table_manager.js
@@ -21,26 +21,3 @@ $(document).ready(function() {
         }
     })
 });
-
-$(document).ready(function() {
-    $("#table_manager").DataTable({
-        "searching": false,
-        "language": {
-            "emptyTable": "Aucun résultat pour votre recherche",
-            "info": "_START_ à _END_ sur _TOTAL_ résultats",
-            "infoEmpty": "0 à 0 sur 0 résultat",
-            "lengthMenu": "_MENU_ résultats par page",
-            "loadingRecords": "Chargement...",
-            "processing": "En cours...",
-            "zeroRecords": "Aucun résultat pour votre recherche",
-            "paginate": {
-                "next": "Suivant",
-                "previous": "Précédent"
-            },
-            "aria": {
-                "sortAscending": ": activate to sort column ascending",
-                "sortDescending": ": activate to sort column descending"
-            }
-        }
-    })
-});
